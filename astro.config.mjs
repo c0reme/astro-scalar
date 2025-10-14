62556a527e62d3f27e8e0e887fa10b0563476dc1
c0reme <cameron.huntley87+github@gmail.com>
2025-10-14 13:00:45 +0100

    feat: add experimental CSP for `is:inline` scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,16 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://c0reme.github.io",
   base: "/scalar-astro",
+  experimental: {
+    csp: {
+      styleDirective: {
+        hashes: ["sha256-7efUq5R3+Li6oE/jTAFS39w/CDIi5kQY2yW0wDTNc+8="],
+      },
+      scriptDirective: {
+        hashes: ["sha256-7efUq5R3+Li6oE/jTAFS39w/CDIi5kQY2yW0wDTNc+8="],
+      },
+    },
+  },
   redirects: {
     "/": "/scalar-astro/api/docs",
   },
